da25c1d59bae8015334874ba1766dcbebd26b85a
Rowii <developerrowi@gmail.com>
2022-07-30 20:13:36 +0800

    Init Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,14 @@ import 'package:pocket_taxi/services/auth.service.dart';
 import 'package:pocket_taxi/services/location.service.dart';
 
 import 'package:pocket_taxi/widgets/colors.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() async {
+Future<void> main() async {
   await WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+      url: 'https://jtdgwvhymxpzxnlhzaoh.supabase.co',
+      anonKey:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0ZGd3dmh5bXhwenhubGh6YW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTc4OTg0MjYsImV4cCI6MTk3MzQ3NDQyNn0.I0wNui0Do171HTwDnDlncubWblUibox3dcdifP41TGc');
   AuthService _authService = AuthService();
   LocationService _locationService = LocationService();
   await _locationService.getCurrentLocation();
